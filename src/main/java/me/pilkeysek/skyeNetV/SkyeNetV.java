80f9f2ef2bafad2165de739c53f8abc2401a1567
PilkeySEK <156579268+PilkeySEK@users.noreply.github.com>
2025-01-03 15:59:31 +0100

    Make skyenetv command

--- src/main/java/me/pilkeysek/skyeNetV/SkyeNetV.java
+++ src/main/java/me/pilkeysek/skyeNetV/SkyeNetV.java
@@ -1,18 +1,47 @@
 package me.pilkeysek.skyeNetV;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.command.CommandManager;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
+import me.pilkeysek.skyeNetV.commands.SkyeNetVCommand;
 import org.slf4j.Logger;
+
+import java.nio.file.Path;
 
 @Plugin(id = "skyenetv", name = "SkyeNetV", version = BuildConstants.VERSION, description = "Velocity Plugin for SkyeNet", url = "store.skyenet.co.in", authors = {"PilkeySEK"})
 public class SkyeNetV {
 
-    @Inject
-    private Logger logger;
+    public final ProxyServer proxy;
+    public final Logger logger;
+    public final Path dataDirectory;
+
+     @Inject
+     public SkyeNetV(ProxyServer proxy, Logger logger, @DataDirectory Path dataDirectory) {
+         this.proxy = proxy;
+         this.logger = logger;
+         this.dataDirectory = dataDirectory;
+     }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+         registerCommands();
+         logger.info("SkyeNetV version " + BuildConstants.VERSION + " initialized.");
+    }
+
+    private void registerCommands() {
+        CommandManager manager = proxy.getCommandManager();
+
+        manager.register(
+                manager.metaBuilder("skyenetv").plugin(this).build(),
+                SkyeNetVCommand.createBrigadierCommand(proxy, this)
+        );
+    }
+
+    public void fullReload() {
+
     }
 }
